85bae18bdb6a27224a728c2735fdc5de5fe91764
Arelthia Phillips <ap.gwhere@gmail.com>
2023-05-29 15:21:58 -0500

    Initial react setup

---
+++ store/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          CupCake Bar
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
